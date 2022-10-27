3ef8bdf4359ad8b97bc273875405eb21208299c6
fan yang <yangfff428@gmail.com>
2022-10-27 16:41:00 +0800

    init project
    (cherry picked from commit 94226b778e7b7cfae6154a9c86b7ef04987b877f)

---
+++ src/test/java/com/xxxx/whiteboard/WhiteBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xxxx.whiteboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiteBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
