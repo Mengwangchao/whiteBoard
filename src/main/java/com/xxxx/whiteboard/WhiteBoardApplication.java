94226b778e7b7cfae6154a9c86b7ef04987b877f
fan yang <yangfff428@gmail.com>
2022-10-27 16:41:00 +0800

    init project

---
+++ src/main/java/com/xxxx/whiteboard/WhiteBoardApplication.java
@@ -0,0 +1,13 @@
+package com.xxxx.whiteboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiteBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhiteBoardApplication.class, args);
+    }
+
+}
