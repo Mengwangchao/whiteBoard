6f353d87d5428f95413c33f8b370ee7a46034c58
fan yang <yangfff428@gmail.com>
2022-10-31 22:00:00 +0800

    完成加入房间、删除房间、房间参数回调（待完善）

--- src/main/java/com/xxxx/whiteboard/pojo/Room.java
+++ src/main/java/com/xxxx/whiteboard/pojo/Room.java
@@ -5,6 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.NotNull;
 
 /**
  * @Author: fan yang
@@ -17,11 +21,16 @@ import lombok.ToString;
 public class Room {
 
     /** 房间号 */
+    @NotNull
+    @Length(min = 12, max = 12)
     private String roomId; //  房间id：123456789011
 
     /** 房间状态 */
     private int roomState; // 房间状态
 
     /** 房间模式 */
+    @Range(min = 1, max = 2)
     private int authority; // 模式：写作和只读
+
+    private int page; // 房间页数
 }
